989b9423eb23c164cde6c8ea77220434e9be07c1
Miguel <cmiguelrb@gmail.com>
2022-02-15 15:04:12 +0100

    Fix blank map.

--- lib/main/home.dart
+++ lib/main/home.dart
@@ -208,6 +208,7 @@ class _Home extends State<Home>
     return WillPopScope(
         onWillPop: () => _onWillPop(context),
         child: Scaffold(
+          resizeToAvoidBottomInset: false,
             body: TabBarView(
                 physics: const NeverScrollableScrollPhysics(),
                 controller: _tabController,
