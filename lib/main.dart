206a1b2f38aa6c632a17fefd4c0c89904eb944f0
Miguel Rodríguez <cmiguelrb@gmail.com>
2022-02-15 02:21:16 +0100

    Firebase Crashlytics.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
@@ -11,6 +13,9 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   final SharedPreferences _prefs = await SharedPreferences.getInstance();
   loadStatusBarTheme(_prefs);
   String? path = _prefs.getString('currentTrack');
