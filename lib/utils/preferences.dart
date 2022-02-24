508befe1a3a3f2563398dd66e14ab66351b68f88
Miguel Rodríguez <cmiguelrb@gmail.com>
2022-02-24 04:22:13 +0100

    Fixes.

--- lib/utils/preferences.dart
+++ lib/utils/preferences.dart
@@ -3,53 +3,55 @@ import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
 
-
-class Preferences{
-
+class Preferences {
   final _fileName = 'preferences';
 
   String themeCurrent = "";
 
-  setThemeCurrent(String current){
+  setThemeCurrent(String current) {
     themeCurrent = current;
   }
 
-  String getThemeCurrent(){
+  String getThemeCurrent() {
     return themeCurrent;
   }
 
   Future<void> set(String key, String value) async {
-    if(key == 'dark_mode'){
+    if (key == 'dark_mode') {
       setThemeCurrent(value);
     }
-    final file = File('${(await getApplicationDocumentsDirectory()).path}/$_fileName.json');
+    final file = File(
+        '${(await getApplicationDocumentsDirectory()).path}/$_fileName.json');
     Map<String, dynamic> prefs;
-    if(!await file.exists()){
+    if (!await file.exists()) {
       prefs = {};
-    }else{
-      prefs = json.decode(await file.readAsString());
+    } else {
+      prefs = json.decode(file.readAsStringSync());
     }
     prefs[key] = value;
-    file.writeAsString(json.encode(prefs));
+    file.writeAsStringSync(json.encode(prefs));
   }
 
   Future<String?> get(String key) async {
-    final file = File('${(await getApplicationDocumentsDirectory()).path}/$_fileName.json');
-    if(!await file.exists()){
+    final file = File(
+        '${(await getApplicationDocumentsDirectory()).path}/$_fileName.json');
+    if (!await file.exists()) {
       return null;
     }
-    String? value = json.decode(await file.readAsString())[key];
-    if(key == 'dark_mode' && value != null){
+    Map<String, dynamic> map = json.decode(file.readAsStringSync());
+    String? value = map[key];
+    if (key == 'dark_mode' && value != null) {
       setThemeCurrent(value);
     }
     return value;
   }
 
-  remove(String key) async{
-    final file = File('${(await getApplicationDocumentsDirectory()).path}/$_fileName.json');
-    Map<String, dynamic> prefs = json.decode(await file.readAsString());
+  Future<void> remove(String key) async {
+    final file = File(
+        '${(await getApplicationDocumentsDirectory()).path}/$_fileName.json');
+    Map<String, dynamic> prefs = json.decode(file.readAsStringSync());
     prefs.remove(key);
-    file.writeAsString(json.encode(prefs));
+    file.writeAsStringSync(json.encode(prefs));
   }
 
   //Singleton pattern. No need to call an instance getter, just instantiate the class Calc calc = Calc();
