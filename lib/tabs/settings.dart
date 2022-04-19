00401242fbf92ebe727dd8cc3bc7de81e78d30bf
Miguel Rodríguez <cmiguelrb@gmail.com>
2022-04-19 20:59:55 +0200

    Push versión.

--- lib/tabs/settings.dart
+++ lib/tabs/settings.dart
@@ -234,7 +234,7 @@ class SettingsState extends State<Settings> {
           ),
           TextButton(
               onPressed: () => _show(lightMode),
-              child: const Text('Vieiros v1.1.0',
+              child: const Text('Vieiros v1.2.0',
                   style: TextStyle(color: CustomColors.faintedText))),
           Container(
             margin: const EdgeInsets.only(bottom: 10),
