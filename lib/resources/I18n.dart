fa29db342ae451dd74e1c986bef65fabed265226
Miguel Rodríguez <cmiguelrb@gmail.com>
2022-04-29 12:19:38 +0200

    Slope gradient WIP

--- lib/resources/I18n.dart
+++ lib/resources/I18n.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 class I18n {
-
   static final _localizations = {
     "en": {
       "common_ok": "OK",
@@ -16,22 +15,19 @@ class I18n {
       "common_close": "Close",
       "common_app_name": "Vieiros",
       "common_share": "Share",
-
       "appbar_tab_tracks": "Tracks",
       "appbar_tab_map": "Map",
       "appbar_tab_info": "Info",
       "appbar_tab_settings": "Configuration",
-
       "app_close_warning_title": "Warning",
       "app_close_warning": "Do you really want to close the app?",
-
       "tracks_search_hint": "Search tracks",
       "tracks_background_tip": "Press + to load a gpx track file",
       "tracks_file_validation_error": "Not a valid gpx file",
       "tracks_unloaded": "Track cleared from the map",
       "tracks_delete_route": "Delete route",
-      "tracks_once_permissions_disclaimer": "Due to the new Android's permission policies, \"Allow all the time\" permission is now needed for tracking the background location. Otherwise, Vieiros won't be able to track your path while being in the background.\n\nYour location data will be only used locally, it's yours, and stored as a GPX track for your own use.",
-
+      "tracks_once_permissions_disclaimer":
+          "Due to the new Android's permission policies, \"Allow all the time\" permission is now needed for tracking the background location. Otherwise, Vieiros won't be able to track your path while being in the background.\n\nYour location data will be only used locally, it's yours, and stored as a GPX track for your own use.",
       "map_waypoint": "Waypoint name",
       "map_channel_name_location": "Background location service",
       "map_notification_title": "Recording track...",
@@ -48,13 +44,13 @@ class I18n {
       "map_voice_notification_in": "in",
       "map_stop_save": "Stop recording and save your track?",
       "map_track_name": "Track name",
-      "map_permissions_request": "This app needs all times location permissions for tracking your position even while working on the background.",
+      "map_permissions_request":
+          "This app needs all times location permissions for tracking your position even while working on the background.",
       "map_grant_permissions": "Grant permissions",
       "map_finish_tracking": "Finish tracking",
       "map_track_pin_start": "Start",
       "map_track_pin_finish": "Finish",
       "map_save_error_file_exists": "File already exists!",
-
       "info_current_track": "Current track",
       "info_loaded_track": "Loaded track",
       "info_total_time": "Total time",
@@ -69,7 +65,6 @@ class I18n {
       "info_altitude_gain": "Gain",
       "info_chart_altitude": "Altitude",
       "info_chart_distance": "Distance",
-
       "settings_title": "Settings",
       "settings_appearance": "Appearance",
       "settings_dark_mode": "Theme",
@@ -78,14 +73,17 @@ class I18n {
       "settings_voice_alerts_desc": "Turns on voice alerts for every kilometer",
       "settings_track": "Track",
       "settings_gradient_polyline": "Loaded track gradient",
-      "settings_gradient_polyline_desc": "Colorful track based on altitude",
+      "settings_gradient_none": "None",
+      "settings_gradient_altitude": "Altitude",
+      "settings_gradient_slope": "Gradient (%)",
       "settings_donate": "Donate",
       "settings_dark_mode_item_system": "System default",
       "settings_dark_mode_item_light": "Light theme",
       "settings_dark_mode_item_dark": "Dark theme",
-      "settings_third_party_thanks": "This application would not exist without the support of these great third-party libraries."
+      "settings_third_party_thanks":
+          "This application would not exist without the support of these great third-party libraries."
     },
-    "es":{
+    "es": {
       "common_ok": "Aceptar",
       "common_cancel": "Cancelar",
       "common_discard": "Descartar",
@@ -98,22 +96,19 @@ class I18n {
       "common_close": "Cerrar",
       "common_app_name": "Vieiros",
       "common_share": "Compartir",
-
       "appbar_tab_tracks": "Rutas",
       "appbar_tab_map": "Mapa",
       "appbar_tab_info": "Info",
       "appbar_tab_settings": "Configuración",
-
       "app_close_warning_title": "Aviso",
       "app_close_warning": "¿Quieres cerrar la apliación?",
-
       "tracks_search_hint": "Buscar rutas",
       "tracks_background_tip": "Presiona + para cargar archivo de ruta gpx",
       "tracks_file_validation_error": "Archivo gpx no válido",
       "tracks_unloaded": "Ruta limpiada del mapa",
       "tracks_delete_route": "Eliminar ruta",
-      "tracks_once_permissions_disclaimer": "A causa de las nuevas directivas de permisos de Android, ahora el permiso \"Permitir siempre\" es requerido para grabar la ubicación en segundo plano. De otra manera, Vieiros no sería capaz de grabar tu camino mientras esté en segundo plano.\n\n Tus datos de ubicación sólo serán usados localmente, son tuyos, y almacenados como ruta GPX para tu propio uso.",
-
+      "tracks_once_permissions_disclaimer":
+          "A causa de las nuevas directivas de permisos de Android, ahora el permiso \"Permitir siempre\" es requerido para grabar la ubicación en segundo plano. De otra manera, Vieiros no sería capaz de grabar tu camino mientras esté en segundo plano.\n\n Tus datos de ubicación sólo serán usados localmente, son tuyos, y almacenados como ruta GPX para tu propio uso.",
       "map_waypoint": "Nombre punto de ruta",
       "map_channel_name_location": "Servicio ubicación segundo plano",
       "map_notification_title": "Grabando ruta...",
@@ -130,13 +125,13 @@ class I18n {
       "map_voice_notification_pace": "Ritmo: ",
       "map_stop_save": "¿Terminar de grabar y guardar la ruta?",
       "map_track_name": "Nombre de la ruta",
-      "map_permissions_request": "Esta app requiere permisos de ubicación en todo momento para obtener tu ubicación incluso cuando la tengas en segundo plano.",
+      "map_permissions_request":
+          "Esta app requiere permisos de ubicación en todo momento para obtener tu ubicación incluso cuando la tengas en segundo plano.",
       "map_grant_permissions": "Conceder permisos",
       "map_finish_tracking": "Terminar grabación",
       "map_track_pin_start": "Comienzo",
       "map_track_pin_finish": "Fin",
       "map_save_error_file_exists": "¡El fichero ya existe!",
-
       "info_current_track": "Ruta actual",
       "info_loaded_track": "Ruta cargada",
       "info_total_time": "Tiempo total",
@@ -151,7 +146,6 @@ class I18n {
       "info_altitude_gain": "Desnivel acumulado",
       "info_chart_altitude": "Altitud",
       "info_chart_distance": "Distancia",
-
       "settings_title": "Ajustes",
       "settings_appearance": "Aspecto",
       "settings_dark_mode": "Tema",
@@ -160,14 +154,17 @@ class I18n {
       "settings_voice_alerts_desc": "Activa alertas de voz por cada kilómetro",
       "settings_track": "Ruta",
       "settings_gradient_polyline": "Gradiente de ruta cargada",
-      "settings_gradient_polyline_desc": "Gradiente de color en función de la altitud",
+      "settings_gradient_none": "Desactivado",
+      "settings_gradient_altitude": "Altitud",
+      "settings_gradient_slope": "Pendiente (%)",
       "settings_donate": "Donar",
       "settings_dark_mode_item_system": "Predeterminado del sistema",
       "settings_dark_mode_item_light": "Tema claro",
       "settings_dark_mode_item_dark": "Tema oscuro",
-      "settings_third_party_thanks": "Esta aplicación no existiría sin el apoyo de estas geniales librerías de terceros."
+      "settings_third_party_thanks":
+          "Esta aplicación no existiría sin el apoyo de estas geniales librerías de terceros."
     },
-    "gl":{
+    "gl": {
       "common_ok": "Aceptar",
       "common_cancel": "Cancelar",
       "common_discard": "Descartar",
@@ -180,22 +177,20 @@ class I18n {
       "common_close": "Pechar",
       "common_app_name": "Vieiros",
       "common_share": "Compartir",
-
       "appbar_tab_tracks": "Rutas",
       "appbar_tab_map": "Mapa",
       "appbar_tab_info": "Info",
       "appbar_tab_settings": "Configuración",
-
       "app_close_warning_title": "Aviso",
       "app_close_warning": "¿Queres pechar a apliación?",
-
       "tracks_search_hint": "Buscar rutas",
-      "tracks_background_tip": "Presiona + para cargares un arquivo de ruta gpx",
+      "tracks_background_tip":
+          "Presiona + para cargares un arquivo de ruta gpx",
       "tracks_file_validation_error": "Arquivo gpx non válido.",
       "tracks_unloaded": "Ruta limpada do mapa",
       "tracks_delete_route": "Eliminar ruta",
-      "tracks_once_permissions_disclaimer": "A causa das novas directivas de permisos de Android, agora o permiso \"Permitir sempre\" é requerido para gravar a localización en segundo plano. Doutra maneria, Vieiros non sería capaz de gravar o teu camiño mentres estea en segundo plano.\n\n Os teus datos de localización só serán usados localmente, son teus, e almacenados coma ruta GPX para o teu propio uso.",
-
+      "tracks_once_permissions_disclaimer":
+          "A causa das novas directivas de permisos de Android, agora o permiso \"Permitir sempre\" é requerido para gravar a localización en segundo plano. Doutra maneria, Vieiros non sería capaz de gravar o teu camiño mentres estea en segundo plano.\n\n Os teus datos de localización só serán usados localmente, son teus, e almacenados coma ruta GPX para o teu propio uso.",
       "map_waypoint": "Nome punto de ruta",
       "map_notification_title": "Gravando ruta...",
       "map_channel_name_location": "Servicio localización segundo plano",
@@ -212,13 +207,13 @@ class I18n {
       "map_voice_notification_pace": "Ritmo: ",
       "map_stop_save": "¿Rematar de gravar e gardar a ruta?",
       "map_track_name": "Nome da ruta",
-      "map_permissions_request": "Esta app require permisos de localización en todo momento para obter a túa localización incluso cando a teñas en segundo plano.",
+      "map_permissions_request":
+          "Esta app require permisos de localización en todo momento para obter a túa localización incluso cando a teñas en segundo plano.",
       "map_grant_permissions": "Conceder permisos",
       "map_finish_tracking": "Rematar gravación",
       "map_track_pin_start": "Comezo",
       "map_track_pin_finish": "Fin",
       "map_save_error_file_exists": "¡O ficheiro xa existe!",
-
       "info_total_current_track": "Ruta actual",
       "info_total_loaded_track": "Ruta cargada",
       "info_total_time": "Tempo total",
@@ -233,7 +228,6 @@ class I18n {
       "info_altitude_gain": "Desnivel acumulado",
       "info_chart_altitude": "Altitude",
       "info_chart_distance": "Distancia",
-
       "settings_title": "Axustes",
       "settings_appearance": "Aspecto",
       "settings_dark_mode": "Tema",
@@ -242,17 +236,22 @@ class I18n {
       "settings_voice_alerts_desc": "Activa alertas de voz por cada quilómetro",
       "settings_track": "Ruta",
       "settings_gradient_polyline": "Gradiente de ruta cargada",
-      "settings_gradient_polyline_desc": "Gradiente de cor en función da altitude",
+      "settings_gradient_none": "Desactivado",
+      "settings_gradient_altitude": "Altitude",
+      "settings_gradient_slope": "Pendente (%)",
       "settings_donate": "Doar",
       "settings_dark_mode_item_system": "Predeterminado do sistema",
       "settings_dark_mode_item_light": "Tema claro",
       "settings_dark_mode_item_dark": "Tema escuro",
-      "settings_third_party_thanks": "Esta aplicación non existiría de non ser polo apoio destas xeniais librarías de terceiros."
+      "settings_third_party_thanks":
+          "Esta aplicación non existiría de non ser polo apoio destas xeniais librarías de terceiros."
     }
   };
 
-  static String translate(String tag){
+  static String translate(String tag) {
     String systemLocale = Platform.localeName.split('_')[0];
-    return _localizations[systemLocale] != null ? _localizations[systemLocale]![tag] ?? tag : _localizations['en']![tag] ?? tag;
+    return _localizations[systemLocale] != null
+        ? _localizations[systemLocale]![tag] ?? tag
+        : _localizations['en']![tag] ?? tag;
   }
 }
