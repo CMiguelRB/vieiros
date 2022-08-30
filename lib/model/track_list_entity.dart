537d0e7068c2aaaee5aa00a7f387e70c59cde622
Miguel Rodríguez <cmiguelrb@gmail.com>
2022-08-30 03:02:51 +0200

    File manager [WIP]

---
+++ lib/model/track_list_entity.dart
@@ -0,0 +1,7 @@
+class TrackListEntity {
+  String name;
+  String? path;
+  bool isFile;
+
+  TrackListEntity({required this.name, required this.path, required this.isFile});
+}
--- lib/utils/files_handler.dart
+++ lib/utils/files_handler.dart
@@ -1,9 +1,6 @@
-import 'dart:convert';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
-import 'package:vieiros/model/gpx_file.dart';
 import 'package:vieiros/utils/permission_handler.dart';
-import 'package:vieiros/utils/preferences.dart';
 
 class FilesHandler {
   Future<String?> writeFile(String gpxString, String name, bool downloads) async {
@@ -14,7 +11,7 @@ class FilesHandler {
         directory = '/storage/emulated/0/Download';
       } else {
         Directory d = await getApplicationDocumentsDirectory();
-        directory = d.path;
+        directory = '${d.path}/tracks';
       }
       String path = '$directory/${name.replaceAll(' ', '_')}.gpx';
       if (!File(path).existsSync()) {
@@ -22,15 +19,6 @@ class FilesHandler {
       } else {
         return '###file_exists';
       }
-      String? jsonString = Preferences().get('files');
-      List<GpxFile> files = [];
-      if (jsonString != null) {
-        files = (json.decode(jsonString) as List)
-            .map((i) => GpxFile.fromJson(i))
-            .toList();
-      }
-      files.insert(0, GpxFile(name: name, path: path));
-      Preferences().set('files', jsonEncode(files));
       return path;
     }
     return null;
@@ -40,8 +28,13 @@ class FilesHandler {
     if(path != null){
       bool hasPermission = await PermissionHandler().handleWritePermission();
       if (hasPermission) {
-        File deleteFile = File(path);
-        deleteFile.delete();
+        if(FileSystemEntity.isFileSync(path)){
+          File deleteFile = File(path);
+          deleteFile.delete();
+        }else{
+          Directory deleteDirectory = Directory(path);
+          deleteDirectory.deleteSync(recursive: true);
+        }
       }
     }
   }
